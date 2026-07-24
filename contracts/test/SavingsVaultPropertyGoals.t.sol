be5cfdfb53c5eb3db8f554d6df1ee5b98de0df4b
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-24 06:26:38 +0300

    modified:   contracts/test/SavingsVaultPropertyGoals.t.sol

--- contracts/test/SavingsVaultPropertyGoals.t.sol
+++ contracts/test/SavingsVaultPropertyGoals.t.sol
@@ -247,8 +247,11 @@ contract SavingsVaultPropertyGoalsTest is Test {
         bool _pastDate
     ) public {
         uint256 targetAmount = _zeroAmount ? 0 : bound(_targetAmount, 1, 1000e18);
+        
+        // Prevent underflow by ensuring we don't subtract more than current timestamp
+        uint256 maxOffset = block.timestamp > 365 days ? 365 days : block.timestamp - 1;
         uint256 targetDate = _pastDate ? 
-            block.timestamp - bound(_targetOffset, 1, 365 days) : 
+            block.timestamp - bound(_targetOffset, 1, maxOffset) : 
             block.timestamp + bound(_targetOffset, 1, 365 days);
         
         // Deposit funds
