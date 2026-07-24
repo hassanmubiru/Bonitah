66f4a59b097ad99e9ca3a0fb610d8a7604bb932c
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-25 00:22:54 +0300

    Bonitah

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -14,7 +14,9 @@ const customJestConfig = {
   },
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/app/globals.css'],
   testMatch: ['**/__tests__/**/*.{js,jsx,ts,tsx}', '**/*(*.)@(spec|test).{js,jsx,ts,tsx}'],
-  transformIgnorePatterns: ['node_modules/(?!(wagmi|@wagmi|@rainbow-me|viem)/)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(wagmi|@wagmi|@rainbow-me|viem|@tanstack|siwe)/)',
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
