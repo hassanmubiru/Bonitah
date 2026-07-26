45b39a62dd76af602368a6d2e3ea0fa39be61c61
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-26 03:44:52 +0300

    Bonitah

--- shared/src/contracts/abis/index.ts
+++ shared/src/contracts/abis/index.ts
@@ -11,27 +11,9 @@ export { EducationABI as educationAbi } from './Education.js';
 export { RegistryABI as registryAbi } from './Registry.js';
 export { GovernanceABI as governanceAbi } from './Governance.js';
 
-// For convenience, also provide default export with all ABIs
-export {
-  SavingsVaultABI,
-  CommunityTreasuryABI,
-  EducationABI,
-  RegistryABI,
-  GovernanceABI,
-} from './SavingsVault.js';
-
-export {
-  CommunityTreasuryABI,
-} from './CommunityTreasury.js';
-
-export {
-  EducationABI,
-} from './Education.js';
-
-export {
-  RegistryABI,
-} from './Registry.js';
-
-export {
-  GovernanceABI,
-} from './Governance.js';
+// Also export original names for convenience
+export { SavingsVaultABI } from './SavingsVault.js';
+export { CommunityTreasuryABI } from './CommunityTreasury.js';
+export { EducationABI } from './Education.js';
+export { RegistryABI } from './Registry.js';
+export { GovernanceABI } from './Governance.js';
