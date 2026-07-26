d4d3bf978ebcc004107b58de13dd00ed002f497c
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-26 17:41:21 +0300

    Bonitah

--- frontend/tests/helpers/mock-wallet.ts
+++ frontend/tests/helpers/mock-wallet.ts
@@ -7,8 +7,7 @@ import { Page, BrowserContext } from '@playwright/test';
  */
 export class MockWallet {
   constructor(
-    private page: Page,
-    private _context: BrowserContext
+    private page: Page
   ) {}
 
   /**
