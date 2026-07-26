b508ff6efddabdedbc5c313b9cd52ee6910cda9b
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-26 13:24:52 +0300

    Bonitah

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -16,8 +16,14 @@ const customJestConfig = {
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts', '!src/app/globals.css'],
   testMatch: ['**/__tests__/**/*.{js,jsx,ts,tsx}', '**/*(*.)@(spec|test).{js,jsx,ts,tsx}'],
   transformIgnorePatterns: [
-    'node_modules/(?!(wagmi|@wagmi|@rainbow-me|viem|@tanstack|siwe)/)',
+    'node_modules/(?!(wagmi|@wagmi|@rainbow-me|viem|@tanstack|siwe|@radix-ui)/)',
   ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
