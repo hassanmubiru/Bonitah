658822b9d509ae20e0eb80c94f8a0fd0b78abd26
hassanmubiru <hassanmubiru7@gmail.com>
2026-07-26 03:44:07 +0300

    Bonitah

--- shared/src/contracts/abis/index.ts
+++ shared/src/contracts/abis/index.ts
@@ -5,25 +5,33 @@
  * Provides type-safe contract ABIs for all BFN contracts
  */
 
-export { savingsVaultAbi } from './SavingsVault.js';
-export { communityTreasuryAbi } from './CommunityTreasury.js';
-export { educationAbi } from './Education.js';
-export { registryAbi } from './Registry.js';
-export { governanceAbi } from './Governance.js';
-
-export {
-  savingsVaultAbi,
-  communityTreasuryAbi,
-  educationAbi,
-  registryAbi,
-  governanceAbi,
-};
+export { SavingsVaultABI as savingsVaultAbi } from './SavingsVault.js';
+export { CommunityTreasuryABI as communityTreasuryAbi } from './CommunityTreasury.js';
+export { EducationABI as educationAbi } from './Education.js';
+export { RegistryABI as registryAbi } from './Registry.js';
+export { GovernanceABI as governanceAbi } from './Governance.js';
 
 // For convenience, also provide default export with all ABIs
-export default {
-  savingsVaultAbi,
-  communityTreasuryAbi,
-  educationAbi,
-  registryAbi,
-  governanceAbi,
-};
+export {
+  SavingsVaultABI,
+  CommunityTreasuryABI,
+  EducationABI,
+  RegistryABI,
+  GovernanceABI,
+} from './SavingsVault.js';
+
+export {
+  CommunityTreasuryABI,
+} from './CommunityTreasury.js';
+
+export {
+  EducationABI,
+} from './Education.js';
+
+export {
+  RegistryABI,
+} from './Registry.js';
+
+export {
+  GovernanceABI,
+} from './Governance.js';
